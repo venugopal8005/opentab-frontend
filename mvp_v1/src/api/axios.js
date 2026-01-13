474f5cdb32705d7702ac5cbc55779ffc1a50ec00
venugopal8005 <22a91a05i5@aec.edu.in>
2026-01-14 02:34:14 +0530

    from the top dist got deleted

--- mvp_v1/src/api/axios.js
+++ mvp_v1/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-// baseURL: "https://opentab-backend.vercel.app",
+  baseURL: "/api",
   withCredentials: true,
 });
 
